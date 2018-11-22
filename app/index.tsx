5f43b025bb066b9bf60930e9352c767e4a6c6be7
shurnster <slainuponmyshield@gmail.com>
2018-11-21 22:22:21 -0800

    Added Browser router

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
 import App from "app/components/app";
@@ -10,9 +11,11 @@ const client = new ApolloClient({
 });
 
 const RootApp: React.SFC = () => (
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>
 );
 
 document.addEventListener("DOMContentLoaded", () => {
