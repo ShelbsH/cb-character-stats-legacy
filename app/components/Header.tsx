29e441936c88bd16f8799484877e7223f915581d
shurnster <slainuponmyshield@gmail.com>
2018-11-28 21:05:01 -0800

    Removed redundant global styles, Added Sidebar and styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { Row, Col, Menu, Input, Icon, Dropdown } from 'antd';
+import {
+  Row,
+  Col,
+  Menu,
+  Input,
+  Icon,
+  Dropdown,
+  Layout,
+  Avatar
+} from 'antd';
+
+type State = {
+  isCollapsed: boolean;
+};
 
 const SearchInput: React.SFC = () => (
   <div className="header-search-container">
@@ -29,43 +42,79 @@ const dropDownMenu = (
   </Menu>
 );
 
-export class Header extends React.Component {
+export class Header extends React.Component<{}, State> {
+  state: State = {
+    isCollapsed: true
+  };
+
+  onCollapsed = () => {
+    this.setState({
+      isCollapsed: !this.state.isCollapsed
+    });
+  };
+
   render() {
+    const { Sider } = Layout;
+    const { isCollapsed } = this.state;
+
     return (
-      <header>
-        <Row className="header-root">
-          <Col
-            lg={6}
-            xl={6}
-            md={6}
-            sm={12}
-            xs={12}
-            className="header-col"
-          >
-            <div className="header-logo-container">
-              <h2 className="header-logo-title">Logo</h2>
-              <Icon type="menu-fold" className="header-logo-menu" />
-            </div>
-          </Col>
-          <Col
-            lg={18}
-            xl={18}
-            md={18}
-            sm={12}
-            xs={12}
-            className="header-col"
-          >
-            <SearchInput />
-            <Dropdown trigger={['click']} overlay={dropDownMenu}>
-              <FontAwesomeIcon
-                icon={faBars}
-                className="menu-bar-icon"
-              />
-            </Dropdown>
-            <HeaderNav />
-          </Col>
-        </Row>
-      </header>
+      <Layout>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={isCollapsed}
+          collapsedWidth={0}
+          className="sidebar"
+        >
+          <div className="sidebar-header">
+            <h1 className="sidebar-header-h1">Profile</h1>
+          </div>
+          <div className="sidebar-profile">
+            <Avatar size={70} icon="user" />
+            <h2 className="sidebar-profile-h2">MarvelFan_102</h2>
+          </div>
+          <Menu mode="inline">
+            <Menu.Item key="1" className="sidebar-menu-item">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2" className="sidebar-menu-item">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3" className="sidebar-menu-item">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <header>
+            <Row className="header-root">
+              <Col md={6} xs={12} className="header-col">
+                <div className="header-logo-container">
+                  <h2 className="header-logo-title">Logo</h2>
+                  <Icon
+                    type={isCollapsed ? 'menu-unfold' : 'menu-fold'}
+                    className="header-logo-menu"
+                    onClick={this.onCollapsed}
+                  />
+                </div>
+              </Col>
+              <Col md={18} xs={12} className="header-col">
+                <SearchInput />
+                <Dropdown trigger={['click']} overlay={dropDownMenu}>
+                  <FontAwesomeIcon
+                    icon={faBars}
+                    className="menu-bar-icon"
+                  />
+                </Dropdown>
+                <HeaderNav />
+              </Col>
+            </Row>
+          </header>
+        </Layout>
+      </Layout>
     );
   }
 }
