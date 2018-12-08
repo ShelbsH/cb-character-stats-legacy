69941e8592cba74e29d521e1b7b8a67fc70f6ba4
shurnster <slainuponmyshield@gmail.com>
2018-12-07 21:32:27 -0800

    changed browser rotuer to hash router for react-router

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 import App from 'app/components/app';
