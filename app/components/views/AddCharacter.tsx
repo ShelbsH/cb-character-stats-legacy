6060900ff3f272622bd78ab9bc6a5ec28ef6ea1a
shurnster <slainuponmyshield@gmail.com>
2018-12-17 13:36:01 -0800

    added Mutation component from react-apollo

--- app/components/views/AddCharacter.tsx
+++ app/components/views/AddCharacter.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Mutation } from 'react-apollo';
+import { ADD_CHARACTER } from 'app/queries';
 import { RootView } from 'app/components/common/RootView';
-import { Formik, Field, FieldProps } from 'formik';
+import { Formik, Field } from 'formik';
 import {
   InputText,
   InputSelect
@@ -15,7 +17,8 @@ const initialValues = {
   powerLevel: '',
   publisher: '',
   powerLevelSelect: ['Street Leveler', 'Powerhouse', 'Skyfather'],
-  publisherSelect: ['Marvel', 'DC']
+  publisherSelect: ['Marvel', 'DC'],
+  abilitiesSelect: []
 };
 
 const validation = Yup.object().shape({
@@ -69,118 +72,145 @@ const uploadButton = (
   </div>
 );
 
+/**
+ * TODO: Refactor all of this code
+ */
+
 export class AddCharacter extends React.Component {
-  //TODO: Reset the form after successful submission
-
-  onFormSubmit = values => {
-    message.info('Your form has been submitted');
-    console.log(values);
-  };
-
   render() {
     const { labelCol, wrapperCol } = formItemLayout;
     return (
       <RootView>
         <h1>Create Character Profile</h1>
         <div className="characterForm">
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validation}
-            validateOnChange={false}
-            validateOnBlur={false}
-            onSubmit={this.onFormSubmit}
-          >
-            {({ values, errors, handleSubmit }) => (
-              <Form layout="horizontal" onSubmit={handleSubmit}>
-                <Row>
-                  <Col lg={12}>
-                    <Field
-                      component={InputText}
-                      name="name"
-                      label="Name"
-                      labelCol={labelCol}
-                      wrapperCol={wrapperCol}
-                      hint="Ex: John or John Doe"
-                      errorMessage={errors.name}
-                      type="text"
-                      hasFeedback
-                    />
-                    <Field
-                      component={InputText}
-                      name="alias"
-                      label="Alias"
-                      labelCol={labelCol}
-                      wrapperCol={wrapperCol}
-                      errorMessage={errors.alias}
-                      type="text"
-                      hasFeedback
-                    />
-                    <Field
-                      component={InputSelect}
-                      name="abilities"
-                      label="Abilities"
-                      select={values.abilitiesSelect}
-                      labelCol={labelCol}
-                      wrapperCol={wrapperCol}
-                      hint="add ability tags"
-                      errorMessage={errors.abilities}
-                      mode="tags"
-                      className="input-tags"
-                      hasFeedback
-                    />
-                    <Field
-                      component={InputSelect}
-                      name="powerLevel"
-                      label="Power Level"
-                      select={values.powerLevelSelect}
-                      labelCol={labelCol}
-                      wrapperCol={wrapperCol}
-                      errorMessage={errors.powerLevel}
-                      className="input-select"
-                      hasFeedback
-                    />
-                    <Field
-                      component={InputSelect}
-                      name="publisher"
-                      label="Publisher"
-                      select={values.publisherSelect}
-                      labelCol={labelCol}
-                      wrapperCol={wrapperCol}
-                      errorMessage={errors.publisher}
-                      className="input-select"
-                      hasFeedback
-                    />
-                  </Col>
-                  <Col lg={12}>
-                    <div className="upload-container">
-                      <Upload
-                        name="avatar"
-                        className="avatar-uploader"
-                        listType="picture-card"
-                        multiple={false}
-                      >
-                        {uploadButton}
-                      </Upload>
-                      <p>
-                        Upload image for avatar (<i>optional</i>)
-                      </p>
-                    </div>
-                  </Col>
-                </Row>
-                <hr className="characterForm-border" />
-                <Form.Item {...tailFormItemLayout}>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    className="btnCreate"
-                    size="large"
-                  >
-                    Create Profile
-                  </Button>
-                </Form.Item>
-              </Form>
-            )}
-          </Formik>
+          <Mutation mutation={ADD_CHARACTER}>
+            {addCharacterProfile => {
+              return (
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={validation}
+                  validateOnChange={false}
+                  validateOnBlur={false}
+                  onSubmit={values => {
+                    //TODO: Reset the form after successful submission
+
+                    const {
+                      name,
+                      alias,
+                      abilities,
+                      powerLevel,
+                      publisher
+                    } = values;
+                    addCharacterProfile({
+                      variables: {
+                        form: {
+                          name,
+                          alias,
+                          abilities,
+                          powerLevel,
+                          publisher
+                        }
+                      }
+                    });
+
+                    message.info('Your form has been submitted');
+                  }}
+                >
+                  {({ values, errors, handleSubmit }) => (
+                    <Form layout="horizontal" onSubmit={handleSubmit}>
+                      <Row>
+                        <Col lg={12}>
+                          <Field
+                            component={InputText}
+                            name="name"
+                            label="Name"
+                            labelCol={labelCol}
+                            wrapperCol={wrapperCol}
+                            hint="Ex: John or John Doe"
+                            errorMessage={errors.name}
+                            type="text"
+                            hasFeedback
+                          />
+                          <Field
+                            component={InputText}
+                            name="alias"
+                            label="Alias"
+                            labelCol={labelCol}
+                            wrapperCol={wrapperCol}
+                            errorMessage={errors.alias}
+                            type="text"
+                            hasFeedback
+                          />
+                          <Field
+                            component={InputSelect}
+                            name="abilities"
+                            label="Abilities"
+                            select={values.abilitiesSelect}
+                            labelCol={labelCol}
+                            wrapperCol={wrapperCol}
+                            hint="add ability tags"
+                            errorMessage={errors.abilities}
+                            mode="tags"
+                            className="input-tags"
+                            hasFeedback
+                          />
+                          <Field
+                            component={InputSelect}
+                            name="powerLevel"
+                            label="Power Level"
+                            select={values.powerLevelSelect}
+                            labelCol={labelCol}
+                            wrapperCol={wrapperCol}
+                            errorMessage={errors.powerLevel}
+                            className="input-select"
+                            hasFeedback
+                          />
+                          <Field
+                            component={InputSelect}
+                            name="publisher"
+                            label="Publisher"
+                            select={values.publisherSelect}
+                            labelCol={labelCol}
+                            wrapperCol={wrapperCol}
+                            errorMessage={errors.publisher}
+                            className="input-select"
+                            hasFeedback
+                          />
+                        </Col>
+                        <Col lg={12}>
+                          <div className="upload-container">
+                            <Upload
+                              name="avatar"
+                              className="avatar-uploader"
+                              listType="picture-card"
+                              multiple={false}
+                            >
+                              {uploadButton}
+                            </Upload>
+                            <p>
+                              Upload image for avatar (<i>optional</i>
+                              )
+                            </p>
+                          </div>
+                        </Col>
+                      </Row>
+                      <hr className="characterForm-border" />
+                      <Form.Item {...tailFormItemLayout}>
+                        <Button
+                          type="primary"
+                          htmlType="submit"
+                          className="btnCreate"
+                          size="large"
+                        >
+                          Create Profile
+                        </Button>
+                      </Form.Item>
+                    </Form>
+                  )}
+                </Formik>
+              );
+            }}
+          </Mutation>
         </div>
       </RootView>
     );
