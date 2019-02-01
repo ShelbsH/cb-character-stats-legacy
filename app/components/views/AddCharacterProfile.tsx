cb422011eb25b459648437a90135c7020e46e1c0
shurnster <slainuponmyshield@gmail.com>
2019-01-31 22:34:05 -0800

    added extra comments for the form and className

--- app/components/views/AddCharacterProfile.tsx
+++ app/components/views/AddCharacterProfile.tsx
@@ -8,7 +8,10 @@ export class AddCharacterProfile extends React.Component {
   render() {
     return (
       <RootView>
-        <h1>Create Character Profile</h1>
+        <h1 className="head-title">Create Character Profile</h1>
+        <p>
+          <i>All fields are required*</i>
+        </p>
         <div className="characterForm">
           {/*TODO: Add error handling when the form is submitted*/}
           <Mutation mutation={ADD_CHARACTER}>
