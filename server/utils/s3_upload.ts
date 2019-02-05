8367f7f103e3e37348e6e60ea63b31766bc6b4be
shurnster <slainuponmyshield@gmail.com>
2019-02-04 23:45:24 -0800

    Added s3 upload utility

---
+++ server/utils/s3_upload.ts
@@ -0,0 +1,31 @@
+import AWS from 'aws-sdk';
+import S3 from 'aws-sdk/clients/s3';
+
+AWS.config.update({
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  region: process.env.AWS_DEFAULT_REGION
+});
+
+let s3 = new S3({
+  apiVersion: '2006-03-01',
+  params: { Bucket: process.env.AWS_BUCKET_NAME }
+});
+
+export const addAvatar = async (filename, stream, fileType) => {
+
+  /**
+   * TODO: Add unique key id, create folder directory
+   * e.g. character/name/avatar
+   */
+
+  return await s3
+    .upload({
+      Key: `avatar/${filename}`,
+      Body: stream,
+      Bucket: 'cb-character-stats',
+      ACL: 'public-read',
+      ContentType: fileType
+    })
+    .promise();
+};
