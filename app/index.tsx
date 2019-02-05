1c5067e064f008c155ee465f73dd123ad656a3e3
shurnster <slainuponmyshield@gmail.com>
2019-02-04 23:43:14 -0800

    added new dependencies for Apollo Client

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { createUploadLink } from 'apollo-upload-client';
 import App from 'app/components/App';
 import 'styles/app.scss';
 
+const url = 'http://localhost:3000/graphql';
+
+const link = new HttpLink({
+  uri: url,
+  credentials: 'same-origin'
+});
+
+const uploadLink = new createUploadLink({
+  uri: url
+});
+
 const client = new ApolloClient({
-  uri: `http://localhost:3000/graphql`
+  link: ApolloLink.from([uploadLink, link]),
+  cache: new InMemoryCache()
 });
 
 const RootApp: React.SFC = () => (
