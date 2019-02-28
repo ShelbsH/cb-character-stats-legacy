08f3b1aca6f931ac2f87e84f240984762b856bc4
shurnster <slainuponmyshield@gmail.com>
2019-02-27 21:20:57 -0800

    Added more testing specs

--- server/__tests__/character.test.ts
+++ server/__tests__/character.test.ts
@@ -2,6 +2,20 @@ import { gql } from 'apollo-server-express';
 import { createTestClient } from 'apollo-server-testing';
 import mongoose from 'mongoose';
 import { testServer } from './test_server';
+import { Character } from '../api/models/character';
+import { data } from '../seeders/seed';
+
+const GET_CHARACTER = gql`
+  query {
+    getCharacterProfiles {
+      name
+      alias
+      abilities
+      powerLevel
+      publisher
+    }
+  }
+`;
 
 const ADD_CHARACTER = gql`
   mutation addCharacterProfile($form: addCharacterProfile!) {
@@ -11,61 +25,115 @@ const ADD_CHARACTER = gql`
   }
 `;
 
-beforeAll(async (done) => {
+describe('Character', () => {
+  beforeAll(async done => {
+    const { readyState } = mongoose.connection;
 
-  const { readyState } = mongoose.connection;
-
-  //Connect with Mongo if it's been disconnected
-  if (readyState === 0) {
-    await mongoose.connect('mongodb://localhost:27017/test', (error) => {
-      if(error) {
-        throw new Error(`There\'s been an error connection, ${error}`);
-      }
-    });
-  }
-  return done();
-});
-
-afterAll(async (done) => {
-  const { db } = mongoose.connection;
-
-  if(db.collection.length) {
-    await db.dropDatabase((error) => {
-      if(error) {
-        throw new Error(error.toString());
-      }
-    });
-  }
-
-  return done();
-});
-
-describe('Mutations', () => {
-  it('should add a new character', async () => {
-    const expected = {
-      addCharacterProfile: {
-        message: 'Character has been added successfully!'
-      }
-    };
-
-    const { server } = await testServer();
-
-    const { mutate } = createTestClient(server as any) as any;
-
-    const res = await mutate({
-      mutation: ADD_CHARACTER,
-      variables: {
-        form: {
-          name: 'John Doe',
-          alias: 'Super Doe',
-          abilities: ['flight', 'laser vision', 'strength', 'speed'],
-          powerLevel: 'Power House',
-          publisher: 'DC',
-          imageUpload: null
+    //Connect with Mongo if it's been disconnected
+    if (readyState === 0) {
+      await mongoose.connect(
+        'mongodb://localhost:27017/test',
+        {
+          useNewUrlParser: true
+        },
+        error => {
+          if (error) {
+            throw new Error(
+              `There\'s been an connection error, ${error}`
+            );
+          }
         }
-      }
+      );
+    }
+    return done();
+  });
+
+  afterAll(async done => {
+    const { db } = mongoose.connection;
+
+    if (db.collection.length) {
+      await db.dropDatabase(error => {
+        if (error) {
+          throw new Error(error.toString());
+        }
+      });
+    }
+
+    return done();
+  });
+
+  describe('Queries', () => {
+    beforeEach(async () => {
+      //Seed some data to the db
+      await Character.collection.insertMany(data);
     });
 
-    expect(res.data).toEqual(expected);
+    it('should display a list of existing characters', async () => {
+      const { server } = await testServer();
+
+      const { query } = createTestClient(server as any);
+
+      const { data } = await query({
+        query: GET_CHARACTER
+      }) as any;
+
+      expect(data.getCharacterProfiles).not.toBeNull();
+      expect(data.getCharacterProfiles).toHaveLength(3);
+    });
+  });
+
+  describe('Mutations', () => {
+    it('adds a new character to the system', async () => {
+      const expected = {
+        addCharacterProfile: {
+          message: 'Character has been added successfully!'
+        }
+      };
+
+      const { server } = await testServer();
+
+      const { mutate } = createTestClient(server as any) as any;
+
+      const res = await mutate({
+        mutation: ADD_CHARACTER,
+        variables: {
+          form: {
+            name: 'John Doe',
+            alias: 'Super Doe',
+            abilities: [
+              'flight',
+              'laser vision',
+              'strength',
+              'speed'
+            ],
+            powerLevel: 'Power House',
+            publisher: 'DC',
+            imageUpload: null
+          }
+        }
+      });
+
+      expect(res.data).toEqual(expected);
+    });
+
+    it('gets the added character from the system', async () => {
+      const expected = {
+        name: 'John Doe',
+        alias: 'Super Doe',
+        abilities: ['flight', 'laser vision', 'strength', 'speed'],
+        powerLevel: 'Power House',
+        publisher: 'DC'
+      };
+
+      const { server } = await testServer();
+
+      const { query } = createTestClient(server as any);
+
+      const { data } = (await query({
+        query: GET_CHARACTER
+      })) as any;
+
+      expect(data.getCharacterProfiles).toContainEqual(expected);
+    });
   });
 });
